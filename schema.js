1f50707d2231d87596aad80e009b3bf5ab68a010
RajiaRani <rajiarani.syal@gmail.com>
2024-01-04 21:31:22 -0600

    updated

--- schema.js
+++ schema.js
@@ -16,3 +16,9 @@ module.exports.listingSchema = Joi.object({
 // module.exports = listingSchema;
 
 //Review side validation
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required()
+})
