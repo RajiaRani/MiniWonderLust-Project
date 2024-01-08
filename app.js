152ad8bb28f9e960f1086a39480d77c7f9b06c65
RajiaRani <rajiarani.syal@gmail.com>
2024-01-08 11:44:24 -0600

    updated

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ app.all("*", (req,res,next) => {
  app.use((err, req, res, next) => {
    let { statusCode = 500, message="Something went wrong!!!" } = err;
     //res.status(statusCode).send(message);
-    res.render("errors.ejs", err);
+    res.render("errors.ejs", err.message);
 
  });
 
