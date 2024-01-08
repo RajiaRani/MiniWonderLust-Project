24078e7d0b9cdcc0153682ffc33a349f5dc3f636
RajiaRani <rajiarani.syal@gmail.com>
2024-01-08 11:05:20 -0600

    updated

--- app.js
+++ app.js
@@ -82,9 +82,9 @@ app.post(
     "/listings", wrapAsync(async(req,res,next) => {
   
     //agar request ki body ke andhar listing nhi hai tab bhi error ayega
-    // if(!req.body.listing) {
-    //     throw new ExpressError(400, "Send valid data for listing");
-    // };
+    if(!req.body.listing){
+        throw new ExpressError(400,"send validate data for listing");
+    };
 
     const newListing = new Listing(req.body.listing);
     await newListing.save();
