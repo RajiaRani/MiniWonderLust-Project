7f1312ff1e97e9aa057ef8f3cc2c4803c221792a
RajiaRani <rajiarani.syal@gmail.com>
2024-01-08 12:32:20 -0600

    updated

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.get("/", (req, res) => {
     let {error} = listingSchema.validate(req.body);
     if(error) {
         let errMsg = error.details.map((el) => el.message).join(",");
-//         throw new ExpressError(400,errMsg);
-//     } else {
-//         next();
-//     };
-// };
+       throw new ExpressError(400,errMsg);
+   } else {
+         next();
+     };
+ };
 
 
 
