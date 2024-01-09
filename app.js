97d42f2d7fbb33adf660d3b32585ddac9802936b
RajiaRani <rajiarani.syal@gmail.com>
2024-01-09 14:51:43 -0600

    updated

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const validateListing = (req,res,next) => {
 const validateReview = ((req,res,next)=> {
     let {error} = reviewSchema.validate(req.body);
     if(error) {
-        let errMsg = error.details.map((el) => el.messagae).join(",");
+        let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError (400, error);
     } else {
         next();
