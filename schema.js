f8dac987377dce617fbdb37d7b5149c7abfd6059
RajiaRani <rajiarani.syal@gmail.com>
2024-01-09 14:19:46 -0600

    updated the show.ejs file

--- schema.js
+++ schema.js
@@ -17,8 +17,5 @@ module.exports.listingSchema = Joi.object({
 
 //Review side validation
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        comment: Joi.string().required(),
-    }).required()
+    review: Joi.object({}).required()
 });
