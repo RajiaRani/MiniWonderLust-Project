38662b3b0ef0ebecc6c448a711884ca6cfe424c0
RajiaRani <rajiarani.syal@gmail.com>
2024-01-08 18:49:59 -0600

    updated

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ const validateListing = (req,res,next) => {
     let {error} = listingSchema.validate(req.body);
     //agar error exist karta hai to
     if(error) {
-        throw new ExpressError(400, error);
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
     } else{
         next();
     };
