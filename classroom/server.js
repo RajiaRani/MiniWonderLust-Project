e382cc086969bf77bfbe7606e0701a041b004552
RajiaRani <rajiarani.syal@gmail.com>
2024-01-14 12:18:22 -0600

    added

--- classroom/server.js
+++ classroom/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const expressSession = require("express-session");
 
+app.use(expressSession, ({secret:"mysuperstring"}));
+
+app.get("/test", (req,res)=> {
+    res.send("test successfull");
+});
 
 app.listen(3000, (req,res) => {
     console.log("i am listing on port 3000");
