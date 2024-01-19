7c3562553f90c1dd5dc193ed2ea098a8694a0a58
RajiaRani <rajiarani.syal@gmail.com>
2024-01-19 10:28:22 -0600

    added user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-//require the local passport
+//require the local passport mongoose to  use the mongoose
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const userSchema = new Schema({
     email:{
         type:String,
