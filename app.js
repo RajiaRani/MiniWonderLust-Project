cdc59362e4519f03b09d390ef3e4aebcaf74acb2
RajiaRani <rajiarani.syal@gmail.com>
2024-01-19 10:35:48 -0600

    added user.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
-
+app.use(passport.session());
   //set the flash as a middleware
   app.use((req,res,next) => {
     res.locals.success = req.flash("success");
