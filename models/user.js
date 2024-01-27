d73701f104bb51373388621691eb27d1959a99b5
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-27 17:02:33 -0600

    added passport features

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 //require the local passport mongoose to  use the mongoose
 const passportLocalMongoose = require("passport-local-mongoose");
 
+//creating schema for User
 const userSchema = new Schema({
     email:{
         type:String,
