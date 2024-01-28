e7eddc71c52a90fbf20d5e5c27a043fa8a012970
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-27 22:35:19 -0600

    added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
@@ -37,6 +38,7 @@ router.get("/login", (req,res) =>{
 
 router.post(
     "/login", 
+    saveRedirectUrl,
     passport.authenticate("local",{
         failureRedirect:"/login", 
         failureFlash:true,
