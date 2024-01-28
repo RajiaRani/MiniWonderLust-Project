cd6ad02d3eee77491af10f96d0169c1067ec92a8
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-27 19:49:49 -0600

    added

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
@@ -27,6 +28,16 @@ router.post("/signup",
 
 router.get("/login", (req,res) =>{
     res.render("users/login.ejs");
-})
+});
+
+router.post(
+    "/login", 
+    passport.authenticate("local",{
+        failureRedirect:"/login", 
+        failureFlash:true,
+    }), 
+    async(req,res) => {
+       res.send("Welcome to Wonderlust you are logged in !");
+    })
 
 module.exports = router;
