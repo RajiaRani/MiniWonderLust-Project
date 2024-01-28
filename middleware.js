be1ea3ff132ed09d74fceb8bdf1feb9f23aaecd5
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-27 22:29:21 -0600

    added

--- middleware.js
+++ middleware.js
@@ -1,10 +1,11 @@
 
 module.exports.isLoggedIn = (req, res, next) => {
-   // console.log(req);
-   console.log(req.path, "..", req.originalUrl);
+    // console.log(req);
+    //console.log(req.path, "..", req.originalUrl);
     //console.log(req.user);
     if (!req.isAuthenticated()) {
-         // return res.redirect("/listings");
+        // return res.redirect("/listings");
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "you must be logged in to create listing");
         return res.redirect("/login");
     }
