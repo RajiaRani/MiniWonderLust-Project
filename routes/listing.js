bd42d702eb44e17c2d866ec2b037d3bc7a408863
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-06 08:23:07 -0600

    added

--- routes/listing.js
+++ routes/listing.js
@@ -39,7 +39,8 @@ router.get(
 
 
 //Step:2 show route
-router.get("/:id", wrapAsync(async (req, res) => {
+router.get("/:id", 
+        wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id)
     .populate("reviews")
