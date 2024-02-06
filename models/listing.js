23ae9b31d00e1c28009d2f8bff95539db26b8504
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-06 17:20:37 -0600

    added

--- models/listing.js
+++ models/listing.js
@@ -9,11 +9,8 @@ const listingSchema = new Schema({
     },
     description: String,
      image:{
-      type: String,
-        default: 
-        "https://images.unsplash.com/photo-1512438925562-6b0f35961d77?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D",
-      set: (v) =>
-         v === " " ? "https://images.unsplash.com/photo-1512438925562-6b0f35961d77?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D": v,
+      url: String,
+      filename: String,
      },
     price: Number,
     location: String,
