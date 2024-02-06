6beff2dbae7b20f0ced4e0de5457ae594bf33034
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-06 14:20:56 -0600

    added

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+const User = require("../models/user");
+
+module.exports.signUp = async(req,res) => {
+    try{
+        let {username, email, password} = req.body;
+        const newUser =  new User({username,email});
+        const registeredUser = await User.register(newUser,password);//user ko register karne ke liye
+        console.log(registeredUser);
+        //automatic login the user
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success","Welcome to Wonderlust!!");
+            res.redirect("/listings");
+        });
+    } catch(e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    };
+};
